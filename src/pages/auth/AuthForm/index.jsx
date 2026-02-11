3e6de8598bd0d3129053efd07b83667ca59aa92a
naeem <naeem.tahir2810@gmail.com>
2026-02-12 00:40:31 +0500

    integrate the sign-up api

--- src/pages/auth/AuthForm/index.jsx
+++ src/pages/auth/AuthForm/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Field from "./Field";
 
 const AuthForm = (props) => {
-  const { fields, buttonText } = props;
+  const { fields, buttonText, onSubmit } = props;
 
   const [formData, setFormData] = useState(() => {
     const initialState = {};
@@ -12,9 +12,19 @@ const AuthForm = (props) => {
     return initialState;
   });
 
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="bg-white border-2 border-slate-200 rounded-md w-68 p-4 shadow-lg font-secondary">
-      <form className="  rounded-md  ">
+      <form
+        className="rounded-md"
+        onSubmit={async (e) => {
+          setLoading(true);
+          e.preventDefault();
+          await onSubmit(formData);
+          setLoading(false);
+        }}
+      >
         {fields.map((field) => (
           <Field
             key={field.label}
@@ -26,8 +36,15 @@ const AuthForm = (props) => {
             }
           />
         ))}
-        <button className="bg-emerald-700 text-white w-full py-1 rounded-md mt-4 ">
+        <button
+          className={`relative bg-emerald-700 text-white w-full py-1 rounded-md mt-4  `}
+        >
           {buttonText}
+          {loading && (
+            <div className="absolute text-black top-0 right-5 h-full flex items-center animate-spin ">
+              <i class="fa-solid fa-spinner text-emerald-100 text-sm"></i>
+            </div>
+          )}
         </button>
       </form>
     </div>
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,11 +1,47 @@
 import { Link } from "react-router-dom";
 import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
+import * as Users from "../../services/user";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const [error, setError] = useState("");
+
+  const submitHandler = async (value) => {
+    setError("");
+
+    if (value.username.length < 4) {
+      setError("username is too short");
+      return;
+    }
+    if (value.password.length < 6) {
+      setError("password too short");
+      return;
+    }
+    if (value.password !== value["confirm password"]) {
+      setError("password must match!");
+      return;
+    }
+
+    try {
+      const res = await Users.creatUser({
+        username: value.username,
+        password: value.password,
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.error);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <FormContainer>
       <div className="flex flex-col items-center justify-center">
+        {error && <p className="text-red-700 text-sm mb-2 ">{error}</p>}
         <AuthForm
           fields={[
             { label: "username", type: "text" },
@@ -13,6 +49,7 @@ const SignUpPage = () => {
             { label: "confirm password", type: "password" },
           ]}
           buttonText="Create Account"
+          onSubmit={submitHandler}
         />
         <Link
           className=" mt-4 underline text-center text-green-700 text-sm"
