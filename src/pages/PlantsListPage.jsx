9cd40d29f53ed6e668a83dd145b4b8a6e1f19fbf
Saira Gulamali <sguacamole98@gmail.com>
2026-03-03 19:14:34 +0000

    add navbar

--- src/pages/PlantsListPage.jsx
+++
@@ -1,5 +0,0 @@
-const PlantsListPage = () => {
-  return <div>This is the plants list page</div>;
-};
-
-export default PlantsListPage;
--- src/shared-components/Navbar/index.jsx
+++ src/shared-components/Navbar/index.jsx
@@ -1,5 +1,30 @@
+// https://static-task-assets.react-formula.com/capstone_logo_light.png
+import SessionContext from "context/SessionContext";
+import { useContext } from "react";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { username } = useContext(SessionContext);
+
+  return (
+    <div className="bg-emerald-600  flex items-center justify-center  ">
+      <div className="  flex w-full max-w-5xl items-center justify-between px-4 py-2">
+        <div className="flex items-center gap-2 ">
+          <img
+            className="h-24 object-contain"
+            src="https://static-task-assets.react-formula.com/capstone_logo_light.png"
+            alt="logo"
+          />
+          <div className="text-white font-primary text-2xl font-medium">
+            Plantify Plants
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <i className="fa-light fa-user text-white text-2xl"></i>
+          <div className="text-white font-primary ">{username}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
