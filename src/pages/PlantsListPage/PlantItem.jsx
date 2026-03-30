cf17f85c31bf5a2decd20553b522092683eaee79
Saira Gulamali <sguacamole98@gmail.com>
2026-03-30 19:41:48 +0100

    make plantlistpage with dynamic colors

---
+++ src/pages/PlantsListPage/PlantItem.jsx
@@ -0,0 +1,66 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+const POT_COLORS = {
+  stone: "bg-stone-200",
+  slate: "bg-slate-300",
+  sky: "bg-sky-700",
+  black: "bg-gray-600",
+  white: "bg-gray-50",
+  amber: "bg-amber-600",
+};
+
+const randomColorIdx = (a) => {
+  const randomIdx = Math.floor(Math.random() * a.length);
+  console.log(randomIdx);
+
+  return randomIdx;
+};
+
+const PlantItem = (props) => {
+  const { plant } = props;
+  const [color, setColor] = useState(() => {
+    return randomColorIdx(plant.images);
+  });
+  // console.log(plant);
+
+  function handleMouseEnter(idx) {
+    setColor(idx);
+  }
+
+  return (
+    <div className="flex flex-col  w-64 text-emerald-700 font-secondary">
+      <img
+        className="rounded-lg"
+        src={plant.images[color].src}
+        alt="plant image"
+      />
+      <div className="flex justify-between my-2 ">
+        <div>{plant.name}</div>
+        <div>${plant.price}</div>
+      </div>
+      <div className="flex justify-between my-2">
+        {plant.images[color].pot_color}
+        <div className="flex justify-end gap-1">
+          {plant.images.map((image, index) => {
+            return (
+              <div
+                key={image.pot_color}
+                onMouseEnter={() => handleMouseEnter(index)}
+                className={clsx(
+                  "h-6 w-6 rounded-full ",
+                  POT_COLORS[image.pot_color],
+                  color === index
+                    ? "outline-2 outline-offset-2 outline-stone-400"
+                    : null,
+                )}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlantItem;
--- src/pages/PlantsListPage/index.jsx
+++ src/pages/PlantsListPage/index.jsx
@@ -1,13 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as PlantsService from "services/plants";
 import Navbar from "shared-components/Navbar";
+import PlantItem from "./PlantItem";
 
 const PlantsListPage = () => {
+  const [plants, setPlants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const getPlants = async () => {
+      setIsLoading(true);
       const response = await PlantsService.getPlants();
       const data = await response.json();
-      console.log(data);
+      setPlants(data);
+      setIsLoading(false);
+      // console.log(data);
     };
     getPlants();
   }, []);
@@ -15,6 +22,21 @@ const PlantsListPage = () => {
   return (
     <div>
       <Navbar />
+      <h1 className="text-3xl font-primary font-bold text-emerald-700 m-8">
+        Plants In Stock
+      </h1>
+      <div className="flex justify-center items-center min-h-screen">
+        {isLoading && (
+          <i className="fa-solid fa-spinner animate-spin text-3xl text-emerald-700 mt-48"></i>
+        )}{" "}
+        <div className="flex  justify-center items-center flex-wrap gap-6 max-w-5xl ">
+          {plants.map((plant) => (
+            <div key={plant.id}>
+              <PlantItem plant={plant} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
