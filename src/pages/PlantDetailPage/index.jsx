0573ee9cb6b249673b266f1d83fc07868d66ed17
naeem <naeem.tahir2810@gmail.com>
2026-04-02 22:29:41 +0500

    use plantDetail api

--- src/pages/PlantDetailPage/index.jsx
+++ src/pages/PlantDetailPage/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "shared-components/Navbar";
+import * as plantService from "services/plants";
+import { useParams } from "react-router-dom";
 
 const PlantDetailPage = () => {
-  return <div>PlantDetailPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const { plantId } = useParams();
+
+  useEffect(() => {
+    const getPlantDetails = async (id) => {
+      setIsLoading(true);
+      const res = await plantService.getPlantDetails(id);
+      const data = await res.json();
+      setIsLoading(false);
+      console.log({ data });
+    };
+
+    getPlantDetails(plantId);
+  }, [plantId]);
+
+  return (
+    <div>
+      <Navbar />
+
+      <main className="bg-emerald-100 min-h-screen flex justify-center">
+        <div className="w-full max-w-5xl border-2 border-red-600"></div>
+      </main>
+    </div>
+  );
 };
 
 export default PlantDetailPage;
