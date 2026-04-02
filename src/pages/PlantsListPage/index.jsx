23431ef8a64fb190b3fadaa3356b73dcec8ab57f
naeem <naeem.tahir2810@gmail.com>
2026-04-02 22:34:54 +0500

    use re-usable spinner component

--- src/pages/PlantsListPage/index.jsx
+++ src/pages/PlantsListPage/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import * as PlantsService from "services/plants";
 import Navbar from "shared-components/Navbar";
 import PlantItem from "./PlantItem";
+import Spinner from "shared-components/Spinner";
 
 const PlantsListPage = () => {
   const [plants, setPlants] = useState([]);
@@ -25,10 +26,8 @@ const PlantsListPage = () => {
       <h1 className="text-3xl font-primary font-bold text-emerald-700 m-8">
         Plants In Stock
       </h1>
-      <div className="flex justify-center items-center min-h-screen">
-        {isLoading && (
-          <i className="fa-solid fa-spinner animate-spin text-3xl text-emerald-700 mt-48"></i>
-        )}{" "}
+      <div className="flex justify-center min-h-screen">
+        {isLoading && <Spinner />}
         <div className="flex  justify-center items-center flex-wrap gap-6 max-w-5xl ">
           {plants.map((plant) => (
             <div key={plant.id}>
