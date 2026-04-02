478b18ddbc0013ce7a5808e421126a1be3f431d8
naeem <naeem.tahir2810@gmail.com>
2026-04-02 22:29:13 +0500

    make plantDetailService

--- src/services/plants.js
+++ src/services/plants.js
@@ -3,3 +3,7 @@ import apiFetch from "./apiFetch";
 export const getPlants = () => {
   return apiFetch("GET", "/plants");
 };
+
+export const getPlantDetails = (id) => {
+  return apiFetch("GET", `/plants/${id}`);
+};
