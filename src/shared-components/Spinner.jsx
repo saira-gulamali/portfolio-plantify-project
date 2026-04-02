0ffcdd50479fd5856d3e23ffae3f91194a654b9e
naeem <naeem.tahir2810@gmail.com>
2026-04-02 22:34:12 +0500

    make Spinner component

---
+++ src/shared-components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <i className="fa-solid fa-spinner animate-spin text-3xl text-emerald-700 mt-24"></i>
+    </div>
+  );
+};
+
+export default Spinner;
