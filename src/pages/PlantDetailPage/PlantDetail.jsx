21a526c77d7b90efdee23b0cf573ad45a96448d9
Saira Gulamali <sguacamole98@gmail.com>
2026-04-06 19:30:07 +0100

    make plant color and image selection dynamic

--- src/pages/PlantDetailPage/PlantDetail.jsx
+++ src/pages/PlantDetailPage/PlantDetail.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import { randomColorIdx } from "utils";
 import BenefitBox from "./BenefitBox";
 import PlantHeader from "./PlantHeader";
 import PlantPurchaseOptions from "./PlantPurchaseOptions";
 
 const PlantDetail = (props) => {
   const { plant } = props;
-  console.log({ plant });
+  const [colorIndex, setColorIndex] = useState(() =>
+    randomColorIdx(plant.images),
+  );
+
   return (
     <div className="flex flex-col md:flex-row pt-8 p-4">
       {/* plant-header for mobile */}
@@ -14,7 +19,7 @@ const PlantDetail = (props) => {
 
       {/* right side */}
       <div className="flex-1">
-        <img className="rounded-lg" src={plant?.images[0]?.src} />
+        <img className="rounded-lg" src={plant?.images[colorIndex]?.src} />
         <div className="flex m-8">
           <BenefitBox
             icon="far fa-check-circle"
@@ -24,8 +29,8 @@ const PlantDetail = (props) => {
           <div className="w-px bg-slate-300 mt-2"></div>
           <BenefitBox
             icon="fa-solid fa-truck-fast"
-            title="Guaranteed Healthy"
-            description="Guaranteed to arrive healthy or your money back"
+            title="Free Shipping"
+            description="Get free ground shipping on orders of $50 or more"
           />
         </div>
       </div>
@@ -38,7 +43,11 @@ const PlantDetail = (props) => {
         <p className="leading-relaxed mt-4 text-slate-600">
           {plant.description}
         </p>
-        <PlantPurchaseOptions plant={plant} />
+        <PlantPurchaseOptions
+          plant={plant}
+          colorIndex={colorIndex}
+          setColorIndex={setColorIndex}
+        />
       </div>
     </div>
   );
--- src/pages/PlantDetailPage/PlantPurchaseOptions.jsx
+++ src/pages/PlantDetailPage/PlantPurchaseOptions.jsx
@@ -1,26 +1,32 @@
-// {
-//     "pot_color": "stone",
-//     "src": "https://static-task-assets.react-formula.com/kimberly_queen_fern_stone.jpeg"
-// }
+import clsx from "clsx";
+import { POT_COLORS } from "utils";
 
 const PlantPurchaseOptions = (props) => {
-  const { plant } = props;
-  console.log(plant);
+  const { plant, colorIndex, setColorIndex } = props;
+
   return (
     <div>
       <div className="mt-6 text-emerald-600 text-2xl flex gap-2 items-center">
         <i className="fa-solid fa-brush "></i>
         <div>Pot Color</div>
       </div>
-      {/* <div>{plant.images[0].pot_color}</div> */}
-      <div className="border-2 border-red-400 flex mt-4">
+
+      <div className=" flex mt-4">
         {plant.images.map((image, idx) => {
           return (
             <div
               key={idx}
               className="flex flex-col justify-center items-center w-14"
             >
-              <div className="w-8 h-8 rounded-full bg-black"></div>
+              <div
+                onMouseEnter={() => setColorIndex(idx)}
+                className={clsx(
+                  "w-8 h-8 rounded-full border-1 border-slate-400",
+                  POT_COLORS[image.pot_color],
+                  colorIndex === idx &&
+                    "outline-2 outline-slate-400 outline-offset-3",
+                )}
+              ></div>
               <div className="text-slate-600 text-center">
                 {image.pot_color}
               </div>
