087ab85bcd2397541a4a6a66f0977b0b6faef354
naeem <naeem.tahir2810@gmail.com>
2026-05-05 22:40:29 +0500

    make remove button

--- src/shared-components/Navbar/modals/CartModal/CartItem.jsx
+++ src/shared-components/Navbar/modals/CartModal/CartItem.jsx
@@ -12,21 +12,32 @@ const CartItem = (props) => {
 
   return (
     <div className="font-secondary flex text-slate-600 mb-8 ">
+      {/* first child */}
       <img className="w-24 mr-4 rounded-md" src={data.image_src} />
-      <div className="flex-1">
-        <div className="flex justify-between font-primary text-xl mb-2">
+
+      {/* 2nd child */}
+      <div className="flex-1 ">
+        <div className="flex justify-between font-primary text-xl mb-2 ">
           <div className=" text-emerald-700">{data.plant_name}</div>
           <div className="text-slate-600">
             ${data.quantity * data.price_per_unit}
           </div>
         </div>
-        <div className="">
+        <div>
           <span className=" text-slate-400 w-12 inline-block">qty:</span>{" "}
           {data.quantity}
         </div>
         <div>
-          <span className=" text-slate-400 w-12 inline-block">color:</span>{" "}
+          <span className=" text-slate-400 w-12 inline-block ">color:</span>
           {data.pot_color}
+        </div>
+        <div className="flex justify-end">
+          <button className=" text-slate-500 hover:text-red-700">
+            remove{" "}
+            <span>
+              <i className="fa-regular fa-trash"></i>
+            </span>
+          </button>
         </div>
       </div>
     </div>
