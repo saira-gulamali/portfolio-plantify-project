be4640e113b76892b49076132ca0841535071997
Saira Gulamali <sguacamole98@gmail.com>
2026-05-11 16:21:23 +0100

    add closeMenu option on background click

--- src/shared-components/Navbar/modals/ModalWrapper.jsx
+++ src/shared-components/Navbar/modals/ModalWrapper.jsx
@@ -1,13 +1,23 @@
+import { useRef } from "react";
 import { RemoveScroll } from "react-remove-scroll";
 
 const ModalWrapper = (props) => {
+  const modalRef = useRef(null);
   const { children, isOpen, closeMenu } = props;
   if (!isOpen) {
     return;
   }
 
   return (
-    <div className=" bg-black/50 h-full  w-full fixed   top-0 left-0 ">
+    <div
+      ref={modalRef}
+      onClick={(e) => {
+        if (e.target === modalRef.current) {
+          closeMenu();
+        }
+      }}
+      className=" bg-black/50  h-full  w-full fixed   top-0 left-0 "
+    >
       <RemoveScroll>
         {/* ---------------------- modal ----------------------  */}
         <button onClick={closeMenu} className="absolute z-10 top-0 right-0 p-4">
