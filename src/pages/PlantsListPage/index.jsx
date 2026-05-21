ee69fa0e8e3c36c9fe18d6a8c1b6bafb42a13a39
naeem <naeem.tahir2810@gmail.com>
2026-05-22 00:38:22 +0500

    animate plant  items (yayyyyy....)

--- src/pages/PlantsListPage/index.jsx
+++ src/pages/PlantsListPage/index.jsx
@@ -3,6 +3,7 @@ import * as PlantsService from "services/plants";
 import Navbar from "shared-components/Navbar";
 import Spinner from "shared-components/Spinner";
 import PlantItem from "./PlantItem";
+import { motion } from "motion/react";
 
 const PlantsListPage = () => {
   const [plants, setPlants] = useState([]);
@@ -28,11 +29,27 @@ const PlantsListPage = () => {
       </h1>
       <div className="flex justify-center min-h-screen">
         {isLoading && <Spinner />}
-        <div className="flex  justify-center items-center flex-wrap gap-6 max-w-5xl ">
-          {plants.map((plant) => (
-            <div key={plant.id}>
+        <div className="flex justify-center items-center flex-wrap gap-6 max-w-5xl ">
+          {plants.map((plant, index) => (
+            //  0 index % 3 = 0 * 0.3 = 0
+            //  1 index % 3 = 1 * 0.3 = 0.3
+            //  2 index % 3 = 2 * 0.3 = 0.6
+            //  3 index % 3 = 0 * 0.3 = 0
+            //  4 index % 3 = 1 * 0.3 = 0.3
+            //  5 index % 3 = 2 * 0.3 = 0.6
+            //  6 index % 3 = 0 * 0.3
+            //  7 index % 3 = 1 * 0.3
+            //  8 index % 3 = 2 * 0.3
+
+            <motion.div
+              key={plant.id}
+              initial={{ opacity: 0, translateY: "100px" }}
+              whileInView={{ opacity: 1, translateY: "0" }}
+              viewport={{ once: true }}
+              transition={{ delay: (index % 3) * 0.2, duration: 0.4 }}
+            >
               <PlantItem plant={plant} />
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
